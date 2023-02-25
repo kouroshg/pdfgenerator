4557fcf9a947bf2fe16c8b0dd584301b249722ee
Kourosh <kourosh06@gmail.com>
2023-02-25 11:21:19 -0800

    running chromium with puppeteer resolver

--- api/testfunc.js
+++ api/testfunc.js
@@ -1,11 +1,17 @@
-const puppeteer = require("puppeteer");
+const PCR = require("puppeteer-chromium-resolver");
+// const puppeteer = require("puppeteer");
 
 module.exports = async (req, res) => {
+  const stats = await PCR({});
   // run a chromium instance with CORS disabled
-  const browser = await puppeteer.launch({
-    headless: false,
-    args: ["--disable-web-security"],
-  });
-
+  const browser = await stats.puppeteer
+    .launch({
+      headless: false,
+      args: ["--disable-web-security", "--no-sandbox"],
+      executablePath: stats.executablePath,
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   res.status(200).send(`Hello, world!`);
 };
