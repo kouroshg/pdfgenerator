85b2df207b611d2b73e43af89fdb4bd01fd5b090
Kourosh <kourosh06@gmail.com>
2023-02-25 10:59:11 -0800

    added puppeteer to testfunc

--- api/testfunc.js
+++ api/testfunc.js
@@ -1,3 +1,11 @@
-module.exports = (req, res) => {
+const puppeteer = require("puppeteer");
+
+module.exports = async (req, res) => {
+  // run a chromium instance with CORS disabled
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--disable-web-security"],
+  });
+
   res.status(200).send(`Hello, world!`);
 };
