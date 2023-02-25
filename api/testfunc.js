6ea74acd2da63ace80e150f50a18dfab96e4fb0f
Kourosh <kourosh06@gmail.com>
2023-02-25 11:39:48 -0800

    changed to puppeteer core and chromium aws lambda

--- api/testfunc.js
+++ api/testfunc.js
@@ -1,19 +1,26 @@
-const PCR = require("puppeteer-chromium-resolver");
-// const puppeteer = require("puppeteer");
+import edgeChromium from "chrome-aws-lambda";
+import puppeteer from "puppeteer-core";
 
-// prepare the chromium instance
-const stats = PCR.getStats({});
+// You may want to change this if you're developing
+// on a platform different from macOS.
+// See https://github.com/vercel/og-image for a more resilient
+// system-agnostic options for Puppeteeer.
+const LOCAL_CHROME_EXECUTABLE =
+  "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
 
 module.exports = async (req, res) => {
+  const executablePath =
+    (await edgeChromium.executablePath) || LOCAL_CHROME_EXECUTABLE;
   // run a chromium instance with CORS disabled
-  const browser = await stats.puppeteer
-    .launch({
-      headless: false,
-      args: ["--disable-web-security", "--no-sandbox"],
-      executablePath: stats.executablePath,
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  const browser = await puppeteer.launch({
+    executablePath,
+    args: edgeChromium.args,
+    headless: false,
+    args: ["--disable-web-security", "--no-sandbox"],
+  });
+
+  const page = await browser.newPage();
+  await page.goto("https://github.com");
+
   res.status(200).send(`Hello, world!`);
 };
