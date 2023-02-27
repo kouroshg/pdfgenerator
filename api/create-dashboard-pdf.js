61c343b0532b2ebc00bbf322197a357e55b2990d
Kourosh <kourosh06@gmail.com>
2023-02-27 11:36:39 -0800

    minor changes

--- api/create-dashboard-pdf.js
+++ api/create-dashboard-pdf.js
@@ -1,14 +1,6 @@
-import edgeChromium from "chrome-aws-lambda";
 import puppeteer from "puppeteer-core";
 import chromium from "chrome-aws-lambda";
 const fs = require("fs").promises;
-
-// You may want to change this if you're developing
-// on a platform different from macOS.
-// See https://github.com/vercel/og-image for a more resilient
-// system-agnostic options for Puppeteeer.
-const LOCAL_CHROME_EXECUTABLE =
-  "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
 
 export default async function (req, res) {
   // replace {{data}} with the data you want to pass to the html page
@@ -17,17 +9,6 @@ export default async function (req, res) {
   const html = await fs.readFile(filePath, "utf8");
   const replacedHtml = html.replace('"{data}"', data);
 
-  // Edge executable will return an empty string locally.
-  const executablePath =
-    (await edgeChromium.executablePath) || LOCAL_CHROME_EXECUTABLE;
-
-  // const browser = await chromium.puppeteer.launch({
-  //   args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
-  //   defaultViewport: chromium.defaultViewport,
-  //   executablePath: await chromium.executablePath,
-  //   headless: true,
-  //   ignoreHTTPSErrors: true,
-  // });
   const browser = await puppeteer.launch({
     args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
     defaultViewport: chromium.defaultViewport,
@@ -35,17 +16,12 @@ export default async function (req, res) {
     headless: true,
     ignoreHTTPSErrors: true,
   });
-  // const browser = await puppeteer.launch({
-  //   executablePath,
-  //   args: edgeChromium.args,
-  //   headless: false,
-  // });
 
   const page = await browser.newPage();
   await page.setViewport({ width: 612, height: 792, deviceScaleFactor: 3 });
   await page.setContent(replacedHtml);
 
-  // we Use pdf function to generate the pdf in the same folder as this file.
+  // get pdf buffer.
   const buffer = await page.pdf({
     format: "letter",
     printBackground: true,
